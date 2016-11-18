864a45d1dcd7bc78fead738647d1bb9303dcda13
Dmitry Kuznetsov <i@aaea.ru>
2016-11-18 14:46:57 +0300

    fix svg

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 'use strict'
 
 require.extensions['.css'] = () => '' // NO CSS!
+require.extensions['.svg'] = () => '' // NO CSS!
 require('babel-core/register')({})
 const path = require('path')
 require(path.join(__dirname, 'server'))
